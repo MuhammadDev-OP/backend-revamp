dd038e28ff3a335f1d7c0a4b33d6f5295993a0de
Muhammad Bangash <muhammadbangash190@gmail.com>
2024-06-23 09:39:34 +0500

    standard setup

--- src/index.js
+++ src/index.js
@@ -9,7 +9,13 @@ dotenv.config({
 
 const app = express();
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`🛞  Server is running at port : ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => console.log(`MONGODB Connection FAILED ${err}`));
 
 // (async () => {
 //   try {
